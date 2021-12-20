6a29adbc1a8392883e2a5d0a5d50ad4f5ee6cef9
Jeonhaeun <21900646@handong.edu>
2021-12-20 15:46:18 +0900

    페이지 생성(개인페이지)

---
+++ lib/day1_member/ee.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EE extends StatefulWidget {
+  const EE({Key? key}) : super(key: key);
+
+  @override
+  _EEState createState() => _EEState();
+}
+
+class _EEState extends State<EE> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/pages/day1.dart
+++ lib/pages/day1.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:advanced/day1_member/ee.dart';
+import 'package:advanced/day1_member/gg.dart';
+import 'package:advanced/day1_member/hh.dart';
+import 'package:advanced/day1_member/yy.dart';
+
 
 class Day1 extends StatefulWidget {
   const Day1({Key? key}) : super(key: key);
@@ -10,6 +15,45 @@ class Day1 extends StatefulWidget {
 class _Day1State extends State<Day1> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(
+                    builder: (BuildContext context) => EE()));
+              },
+              child: Text('규경'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(
+                    builder: (BuildContext context) => GG()));
+              },
+              child: Text('은진'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(
+                    builder: (BuildContext context) => HH()));
+              },
+              child: Text('유리'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(
+                    builder: (BuildContext context) => YY()));
+              },
+              child: Text('하은'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
